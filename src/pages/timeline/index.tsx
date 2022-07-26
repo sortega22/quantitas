75ecccfbbfd536eb19bb9bca29b26f153f3bc892
Alfredo Jose <alfredo.bertho@infracommerce.lat>
2022-07-26 15:48:23 -0300

    fix: styles

--- src/pages/timeline/index.tsx
+++ src/pages/timeline/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import style from './styles.module.scss';
 
-// import { Container } from './styles';
+export async function getStaticProps() {
+    return { props: { isDark: true } };
+  }
 
 const timeline = () => {
   return (
@@ -9,40 +11,46 @@ const timeline = () => {
     <div className={style.container}>
         <ol className={style.pstimeline}>
             <li>
-                <div className={style.imghandlertop}>
-                    <img src="http://www.physology.co.uk/wp-content/uploads/2016/02/ps-elem_03.png" alt=""/>
-                </div>
                 <div className={style.psbot}>
                     <p>Do you have a recent injury or long term pain?</p>
                 </div>
-                <span className={style.pssptop}>01</span>
+                <span className={style.psspbot}>2011</span>
             </li>
             <li>
-                <div className={style.imghandlerbot}>
-                    <img src="http://www.physology.co.uk/wp-content/uploads/2016/02/ps-elem_13.png" alt=""/>
-                </div>
                 <div className={style.pstop}>
                     <p>Have you tried Physiotherapy, Chiropractor or your GP without the pain free results?</p>
                 </div>
-                <span className={style.psspbot}>02</span>
+                <span className={style.pssptop}>2014</span>
             </li>
             <li>
-                <div className={style.imghandlertop}>
-                    <img src="http://www.physology.co.uk/wp-content/uploads/2016/02/ps-elem_05.png" alt=""/>
-                </div>
                 <div className={style.psbot}>
                     <p>Let Physology assess and treat your pain with our trusted revolusionary approach.</p>
                 </div>
-                <span className={style.pssptop}>03</span>
+                <span className={style.psspbot}>2016</span>
             </li>
             <li>
-                <div className={style.imghandlerbot}>
-                    <img src="http://www.physology.co.uk/wp-content/uploads/2016/02/ps-elem_10.png" alt=""/>
-                </div>
                 <div className={style.pstop}>
                     <p>Join our happy family of pain free clients.</p>
                 </div>
-                <span className={style.psspbot}>04</span>
+                <span className={style.psspbot}>2018</span>
+            </li>
+            <li>
+                <div className={style.pstop}>
+                    <p>Join our happy family of pain free clients.</p>
+                </div>
+                <span className={style.psspbot}>2019</span>
+            </li>
+            <li>
+                <div className={style.pstop}>
+                    <p>Join our happy family of pain free clients.</p>
+                </div>
+                <span className={style.psspbot}>2019</span>
+            </li>
+            <li>
+                <div className={style.pstop}>
+                    <p>Join our happy family of pain free clients.</p>
+                </div>
+                <span className={style.psspbot}>2020</span>
             </li>
         </ol>
     </div>
