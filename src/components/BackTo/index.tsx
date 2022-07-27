5f8f4b71af445fcd0e0c06d5b94a74bc17c2b799
Alfredo Jose <alfredo.bertho@infracommerce.lat>
2022-07-27 15:42:33 -0300

    fix: add setas

--- src/components/BackTo/index.tsx
+++ src/components/BackTo/index.tsx
@@ -3,15 +3,35 @@ import Image from 'next/image'
 import style from './styles.module.scss';
 import Link from 'next/link';
 import Shake from 'react-reveal/Shake';
+import Lottie from 'react-lottie-player'
+import lottieJson from '../../setas.json'
+
+
+// const BackTo = () => {
+//   return (<>
+//     <Link href={"/home"}>
+//       <a>
+//         <div className={style.backContainer}>
+//           <Shake>
+//             <Image src={"/icons/seta.png"} width={82} height={82} alt="seta" />
+//           </Shake>
+//         </div>
+//       </a>
+//     </Link>
+//   </>);
+// }
 
 const BackTo = () => {
   return (<>
     <Link href={"/home"}>
       <a>
-        <div className={style.backContainer}>
-          <Shake>
-            <Image src={"/icons/seta.png"} width={82} height={82} alt="seta" />
-          </Shake>
+      <div className={style.backContainer}>
+         <Lottie
+            loop
+            animationData={lottieJson}
+            play
+            style={{ width: 70, height: 70 }}
+          />
         </div>
       </a>
     </Link>
--- src/components/BoxContent/index.tsx
+++ src/components/BoxContent/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Image from 'next/image'
 import style from './styles.module.scss';
 import Shake from 'react-reveal/Shake';
+import Lottie from 'react-lottie-player';
+import lottieJson from '../../setas.json'
 
 interface BoxContentProps {
   iconName: string;
@@ -27,9 +29,16 @@ const BoxContent: React.FC<BoxContentProps> = ({
           <Image src={`/icons/${iconName}.svg`} className={`${style.boxIcon}`} height={118} width={118} alt={title}/>
           <div className={`${style.boxContentTitle}`}>
             {!isDiferent ? <h2>{title} <span>{titleLink}</span></h2> : <h2>{titleLink} {title} <span>{titleContato}</span></h2>}
-            <Shake>
-              <Image src={"/icons/seta.png"} width={82} height={82} alt="seta" />
-            </Shake>
+            <div style={{
+              paddingBottom: '15px'
+            }}>
+              <Lottie
+                loop
+                animationData={lottieJson}
+                play
+                style={{ width: 70, height: 70, marginTop: '-10px', transform: 'rotate(270deg)' }}
+              />
+            </div>
           </div>
         </div>
       </div>
